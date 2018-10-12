7eba11db2622d6b278d80d8bb2d02f3c9ee0c46d
IrineZalesskaya <zalesskaya.ia@gmail.com>
2018-10-12 22:20:57 +0300

    Add files via upload

---
+++ js1/main.js
@@ -0,0 +1,40 @@
+var c1=2,
+    c2='Officiis laudantium nisi eum deserunt',
+    c3=true,
+    c4=[1,2,3,4];
+    c5= {
+    	day:5,
+    	month:'7',
+    	year:12
+    } 
+
+    console.log(c1);
+    console.log(c2);
+    console.log(c3);
+    console.log(c4);
+    console.log(c5);
+
+    console.log(c1.toFixed(2));
+
+    var i=8;
+    console.log(++i);
+ 
+    console.log(i++);
+    console.log(c1+=5);
+    console.log(c1-=3);
+    console.log(c1*=2);
+    console.log(c1/=2);
+    console.log(c1%=3);
+    console.log(Math.PI);
+    console.log(Math.round(89.279));
+    console.log(Math.random());
+    console.log(Math.pow(2,5));
+    console.log(c2.length);
+    console.log(c2.substring(7, 15));
+    console.log(c2.replace('eum', 'nisi'));
+    console.log(c2.toUpperCase());
+    console.log(c2.toLowerCase());
+
+
+
+
